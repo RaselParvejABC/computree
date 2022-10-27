3fbce9c0832c7ac4ee964446deef794b3e356d24
Rasel Parvej <RaselParvejABC@gmail.com>
2022-10-27 17:26:41 +0600

    Login Form and Register Form Created and Connected; Now to implement Functionality

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -8,17 +8,21 @@ export default function ErrorPage() {
     <div className="h-screen w-screen bg-gray-100 flex items-center">
       <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
         <div className="max-w-md">
-          <div className="text-5xl font-dark font-bold">404</div>
+          <div className="text-5xl font-dark font-bold">Oh! Snap!</div>
           <p className="text-2xl md:text-3xl font-light leading-normal">
-            Sorry we couldn't find this page.{" "}
+            Sorry, Something went wrong! Please, Check Web Console for Technical
+            Details of the Error.
           </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
+          <p className="my-8">
+            But don't worry, you can find plenty of other things on our
+            homepage.
           </p>
 
-          <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
-            back to homepage
-          </button>
+          <Link to="/">
+            <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
+              Back to Home
+            </button>
+          </Link>
         </div>
         <div className="max-w-lg"></div>
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,111 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
+        <div>
+          <h3 className="text-4xl font-bold text-black">CompuTree</h3>
+        </div>
+        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+          <form>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Full Name
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="text"
+                  name="name"
+                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="pic"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Profile Pic URL
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="text"
+                  name="pic"
+                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Email
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="email"
+                  name="email"
+                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Password
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="password"
+                  name="password"
+                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                htmlFor="password_confirmation"
+                className="block text-sm font-medium text-gray-700 undefined"
+              >
+                Confirm Password
+              </label>
+              <div className="flex flex-col items-start">
+                <input
+                  type="password"
+                  name="password_confirmation"
+                  className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center mt-4">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Register
+              </button>
+            </div>
+          </form>
+          <div className="mt-4 text-grey-600 text-center">
+            Already have an account?
+            <br />
+            Or, want to login with Google/Github? <br />
+            <span>
+              <Link className="text-purple-600 hover:underline" to="/login">
+                Log in
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
